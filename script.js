891345516f73d2499e3cd6725bc2dfcae39b44f0
Jaswanth <147476570+jashwanth6215@users.noreply.github.com>
2025-08-05 22:21:42 +0530

    Update script.js
    The autoplay simulates continuous battles between the player and computer.
    
    The autoplay button toggles between "Play" and "Stop" using a single click.
    
    Players can click buttons or press R, P, or S to play their move.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const score=JSON.parse(localStorage.getItem('score'))||{
     tie:0
 };
 let scoresEle=document.querySelector('.js-scores');
-scoresEle.innerHTML='Wins: '+score.win+' , Losses: '+score.loss+' ,Ties: '+score.tie;
+scoresEle.innerHTML='Wins : '+score.win+' , Losses : '+score.loss+' ,Ties : '+score.tie;
 function ran()
 {
     const random=Math.random();
@@ -38,14 +38,50 @@ function play(user)
     localStorage.setItem('score',JSON.stringify(score));
     display(res,user,computer);
 }
+document.body.addEventListener('keydown',(event)=>{
+    if(event.key==='r')
+    {
+        play('Rock');
+    }
+    else if(event.key==='p')
+    {
+        play('Paper');
+    }
+    else if (event.key==='s'){
+        play('Scissors');
+    }
+})
 function display(res,user,computer)
 {
     let resultEle=document.querySelector('.js-result');
     resultEle.innerHTML='You '+res+'.';
     let moveEle=document.querySelector('.js-move');
-    moveEle.innerHTML=`You <img src="images/${user}.png">  Computer <img src="images/${computer}.png">`;
+    moveEle.innerHTML=`You <img src="images/${user}.png"> <span></span> Computer <img src="images/${computer}.png">`;
     let scoresEle=document.querySelector('.js-scores');
     scoresEle.innerHTML='Wins: '+score.win+' , Losses: '+score.loss+' ,Ties: '+score.tie;
+    const userButton = document.querySelector(`.${user}`);
+    userButton.classList.remove("anime");
+    void userButton.offsetWidth;
+    userButton.classList.add("anime"); 
+}
+let intervalId;
+function autoplay()
+{
+    
+    let au=document.querySelector(".js-auto");
+    if(au.innerText==='Auto play')
+    {
+        intervalId=setInterval(function nostop(){
+            let user=ran();
+            play(user);
+        },1000);
+        au.innerText="Stop play"
+    }
+    else{
+        clearInterval(intervalId);
+        au.innerText="Auto play";    
+    }
+    
 }
 function reset()
 {
